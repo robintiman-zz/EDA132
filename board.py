ace174f9e2dea89f4c491cad0a4c4619d497ce68
Robin Timan <robintiman@gmail.com>
2017-02-03 12:34:05 +0100

    First

---
+++ board.py
@@ -0,0 +1,19 @@
+class Board():
+
+    def __init__(self):
+        pass
+
+    def placeTile(self, x, y):
+        pass
+
+    def swapTile(self, x, y):
+        pass
+
+class Tile:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+
