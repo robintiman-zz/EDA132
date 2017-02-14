19f3e13e8af998ad57eea28158e7d3df149b5b09
Robin Timan <robintiman@gmail.com>
2017-02-14 19:22:42 +0100

    Started ass 2

---
+++ Assignment 2/GradientDescent.py
@@ -0,0 +1,34 @@
+# English
+total_letters = [35680, 42514, 15162, 35298, 29800, 40255, 74532, 37464, 31030, 24843, 36172,
+                 39552, 72545, 75352, 18031]
+total_a = [2217, 2761, 990, 2274, 1865, 2606, 4805, 2396, 1993, 1627, 2375, 2560, 4597, 4871, 1119]
+
+class GradientDescent:
+
+    def __init__(self, learning_rate):
+        self.learning_rate = learning_rate
+
+
+
+def main():
+    gd = GradientDescent()
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
