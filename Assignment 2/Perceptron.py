08c1121ee1be48e8b0ed012aefd55ae6798df915
Robin Timan <robintiman@gmail.com>
2017-02-16 00:02:35 +0100

    Added text files according to the LIBSVM format. Which sucks ass

---
+++ Assignment 2/Perceptron.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+class Perceptron:
+    # Stochastic learning is used
+
+    def __init__(self):
+        # English = 0, French = 1
+        pass
+        # self.x_vector = np.array([1, x1, x2])
+        # self.weights = np.array([w0, w1, w2])
+
+    def update_weights(self):
+        pass
+
+
+def main():
+    Perceptron()
+
+if __name__=="__main__":
+    main()
