4212bd7b839f7be57b641e244070e0dd5533324b
Robin Timan <robintiman@gmail.com>
2017-02-17 00:13:25 +0100

    Pereptron on its way

--- Assignment 2/GradientDescent.py
+++ Assignment 2/GradientDescent.py
@@ -6,13 +6,9 @@ BATCH = 2
 
 class GradientDescent:
 
-    def __init__(self, mode, learning_rate, eps):
-        # English
-        self.total_letters = np.array([35680, 42514, 15162, 35298, 29800, 40255, 74532, 37464, 31030, 24843, 36172,
-                                       39552, 72545, 75352, 18031])
-        self.total_a = np.array([2217, 2761, 990, 2274, 1865, 2606, 4805, 2396, 1993, 1627,
-                                 2375, 2560, 4597, 4871, 1119])
-
+    def __init__(self, total_letters, total_a, mode, learning_rate, eps):
+        self.total_letters = total_letters
+        self.total_a = total_a
         self.learning_rate = learning_rate
         self.scale_values()
         self.eps = eps
@@ -34,7 +30,6 @@ class GradientDescent:
         divider = 100000
         self.total_letters = self.total_letters / divider
         self.total_a = self.total_a / divider
-        print(self.total_letters)
 
     def regression(self):
         """
@@ -72,7 +67,7 @@ class GradientDescent:
         if self.mode == BATCH:
             return np.abs(loss0) < self.eps and np.abs(loss1) < self.eps
         else:
-            return np.abs((loss0 - loss0_old))**2 < self.eps and np.abs((loss1 - loss1_old))**2 < self.eps
+            return np.abs(loss0 - loss0_old) < self.eps**2 and np.abs(loss1 - loss1_old) < self.eps**2
 
     def grad_loss_w0(self, w0, w1, mode, index = None):
         """
@@ -84,7 +79,7 @@ class GradientDescent:
         loss_sum = 0
         if mode == BATCH:
             nbr_samples = len(self.total_letters)
-            tmp = np.subtract(self.total_letters, w1*self.total_a + w0)
+            tmp = np.subtract(self.total_letters, w1 * self.total_a + w0)
             loss_sum = -2*np.sum(tmp)
             return loss_sum/nbr_samples
         else:
@@ -101,7 +96,7 @@ class GradientDescent:
         loss_sum = 0
         if mode == BATCH:
             nbr_samples = len(self.total_letters)
-            tmp = np.subtract(self.total_letters, w1*self.total_a + w0)
+            tmp = np.subtract(self.total_letters, w1 * self.total_a + w0)
             tmp = np.multiply(tmp, self.total_a)
             loss_sum = -2*np.sum(tmp)
             return loss_sum/nbr_samples
@@ -111,8 +106,24 @@ class GradientDescent:
             return loss_sum
 
 def main():
-    gd = GradientDescent(STOCH, 0.1, 0.00001)
-    x = np.linspace(0, 0.05, 100)
+    # English
+    total_en = np.array([35680, 42514, 15162, 35298, 29800, 40255, 74532, 37464, 31030, 24843, 36172,
+                                   39552, 72545, 75352, 18031])
+    total_a_en = np.array([2217, 2761, 990, 2274, 1865, 2606, 4805, 2396, 1993, 1627,
+                             2375, 2560, 4597, 4871, 1119])
+
+    # French
+    total_fr = np.array([36961, 43621, 15694, 36231, 29945, 40588, 75255, 37709,
+                              30899, 25486, 37497, 40398, 74105, 76725, 18317])
+    total_a_fr = np.array([2503, 2992, 1042, 2487, 2014, 2805, 5062, 2643, 2126, 1784, 2641, 2766,
+                                5047, 5312, 1215])
+    gd = GradientDescent(total_en, total_a_en, STOCH, 0.1, 0.00001)
+    x = np.linspace(0, 0.06, 100)
+    plt.plot(x, gd.func(x))
+    plt.scatter(gd.total_a, gd.total_letters)
+
+    gd = GradientDescent(total_fr, total_a_fr, STOCH, 0.1, 0.00001)
+    x = np.linspace(0, 0.06, 100)
     plt.plot(x, gd.func(x))
     plt.scatter(gd.total_a, gd.total_letters)
     plt.show()
--- Assignment 2/Perceptron.py
+++ Assignment 2/Perceptron.py
@@ -1,20 +1,63 @@
 import numpy as np
+from builtins import print
+
 
 class Perceptron:
     # Stochastic learning is used
 
-    def __init__(self):
+    def __init__(self, alpha):
         # English = 0, French = 1
-        pass
+        self.total_en = np.array([35680, 42514, 15162, 35298, 29800, 40255, 74532, 37464, 31030, 24843, 36172,
+                             39552, 72545, 75352, 18031])
+        self.total_a_en = np.array([2217, 2761, 990, 2274, 1865, 2606, 4805, 2396, 1993, 1627,
+                               2375, 2560, 4597, 4871, 1119])
+        # self.reader()
+        self.alpha = alpha
         # self.x_vector = np.array([1, x1, x2])
         # self.weights = np.array([w0, w1, w2])
 
-    def update_weights(self):
+    def update(self):
+        # Use np.dot
         pass
+
+    def scale_values(self):
+        """
+        :return: Scaled input array
+        """
+        divider = 100000
+        self.total_en = self.total_en / divider
+        self.total_a_en = self.total_a_en / divider
+
+    def update_alpha(self, t):
+        self.alpha = 1000 / (1000 + t)
+
+    def reader(self):
+        # Martin gör detta om du vill
+        french = open('french.txt', 'r')
+        classes = []
+        for line in french:
+            tmp = line.split(" ")
+            classes[0] = int(tmp[0])
+
+    def predict(self, weights, x_vector):
+        if weights * x_vector >= 0:
+            return 1
+        else:
+            return 0
+
+    def loss(self, y, y_hat):
+        """
+        Loss function
+        :param y: The real class
+        :param y_hat: The predicted class
+        :return: 0 if correct prediction, 1 or -1 otherwise
+        """
+        return y - y_hat
+
 
 
 def main():
-    Perceptron()
+    Perceptron(0.5)
 
 if __name__=="__main__":
     main()
