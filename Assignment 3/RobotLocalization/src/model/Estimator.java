c961b37535a4c96112b286216360dda45b240a48
Robin Timan <robintiman@gmail.com>
2017-03-03 15:57:19 +0100

    Transition done and verified

--- Assignment 3/RobotLocalization/src/model/Estimator.java
+++ Assignment 3/RobotLocalization/src/model/Estimator.java
@@ -2,6 +2,8 @@ package model;
 
 import control.EstimatorInterface;
 
+import java.util.Random;
+
 public class Estimator implements EstimatorInterface {
     private int rows, cols, head;
     private int[] truePos;
@@ -10,7 +12,8 @@ public class Estimator implements EstimatorInterface {
     private static final int WEST = 1;
     private static final int SOUTH = 2;
     private static final int EAST = 3;
-    private static final double NOTHING = -1;
+    private static final int FIRSTCORNER = 3, FIRSTWALL = 5, FIRSTNOWALL = 8;
+//    private static final int NOTHING = -1, FOUND = 1;
 
     public Estimator(int rows, int cols, int head) {
         this.rows = rows;
@@ -21,7 +24,8 @@ public class Estimator implements EstimatorInterface {
         T = new double[S][S];
         O = new double[S][S];
         fillTransitionMatrix();
-        fillObservationMatrix();
+        int[] reading = sensor();
+//        fillObservationMatrix();
 
         for (int i = 0; i < T.length; i++) {
             for (int j = 0; j < T[i].length; j++) {
@@ -70,30 +74,125 @@ public class Estimator implements EstimatorInterface {
 
     // -------- PRIVATE METHODS --------
 
-    private void fillObservationMatrix() {
+    //  The sensor reports
+// - the true location L with probability 0.1
+// - any of the n_Ls ∈ {3, 5, 8} existing surrounding fields L_s with probability 0.05 each.
+// - any of the n_Ls2 ∈ {5, 6, 7, 9, 11, 16} existing “secondary” surrounding fields L_s2 with probability 0.025 each
+// - "nothing" with probability 1.0 - 0.1 - n_Ls*0.05 - n_Ls2*0.025
+    private int[] sensor() {
+        int truepos = convertToLinearPos(truePos[0], truePos[1]);
+        int[] walls = encounteringWalls(truepos, -1);
+        int n_Ls, n_Ls2;
+        int nbrOfSurroundingWalls = walls[1];
+        if (nbrOfSurroundingWalls == 0) {
+            // This is hard-coded for now and assumes a 4x4 grid.
+            n_Ls = FIRSTCORNER;
+            n_Ls2 = 5;
+        } else if (nbrOfSurroundingWalls == 1) {
+            n_Ls = FIRSTWALL;
+            n_Ls2 = 6;
+        } else {
+            n_Ls = FIRSTNOWALL;
+            n_Ls2 = 7;
+        }
+        double probForSurrField = 0.05 * n_Ls;
+        double probFor2SurrField = 0.025 * n_Ls2;
+        double probForTruePos = 0.1;
+        double probForNothing = 1.0 - probForSurrField - probFor2SurrField - probForTruePos;
+        Random rand = new Random();
+        double reading = rand.nextDouble();
+        if (reading <= probForTruePos) {
+            return truePos;
+        } else if (reading <= probForTruePos + probForSurrField) {
+            // Return some pos in the directly surrounding field.
+            return null;
+        } else if (reading <= probForTruePos + probForSurrField + probFor2SurrField) {
+            // Return some pos in the second surrounding field.
+
+            return null;
+        } else {
+            // Return nothing (null)
+            return null;
+        }
+
+    }
+
+//    private int[] pickFromSurroundingField(int level, int n) {
+//        int x = truePos[0];
+//        int y = truePos[1];
+//        Random rand = new Random();
+//        int step = 3;
+//        if (level == 1) {
+//            // First surrounding field
+//            if (n == FIRSTCORNER) {
+//                step = 2;
+//                if (x == 0) {
+//                    x += rand.nextInt(step);
+//                } else {
+//                    x -= rand.nextInt(step);
+//                }
+//                if (y == 0) {
+//                    y += rand.nextInt(step);
+//                } else {
+//                    y -= rand.nextInt(step);
+//                }
+//            } else if (n == FIRSTWALL) {
+//                step = 2;
+//                if (x == 0) {
+//                    x += rand.nextInt(step);
+//                } else if (x == cols) {
+//                    x -= rand.nextInt(step);
+//                } else {
+//                    x += rand.nextInt(step + 1) - 1;
+//                }
+//                if (y == 0) {
+//                    x += rand.nextInt(step);
+//                } else if (x == cols) {
+//                    x -= rand.nextInt(step);
+//                } else {
+//                    x += rand.nextInt(step + 1) - 1;
+//                }
+//            }
+//        } else {
+//            // Second surrounding field
+//
+//        }
+//    }
+
+    /**
+     * Fills the O matrix
+     * @param reading True if the sensor found something, False otherwise.
+     */
+    private void fillObservationMatrix(boolean reading) {
         int pos, realPos;
         double prob;
         for (int i = 0; i < O[0].length; i += head) {
             pos = i / head;
             realPos = convertToLinearPos(truePos[0], truePos[1]);
-            if (pos == realPos) {
-                prob = 0.1;
-            } else if ()
-
-
-            O[i][i] = prob;
+//            if (reading) {
+//                if (pos == realPos) {
+//                    prob = 0.1;
+//                } else if () {
+//
+//                }
+//
+//            } else {
+//
+//            }
+//            O[i][i] = prob;
         }
     }
 
     private int getNbrOfSurroundingFields(int pos) {
         // We only want the number of walls so heading doesn't matter here.
         int[] walls = encounteringWalls(pos, -1);
+        return 0;
 
     }
 
     private boolean inDirectlySurroundingFields(int pos, int realPos) {
         int diff = Math.abs(pos - realPos);
-        
+        return true;
     }
 
     /**
@@ -112,21 +211,25 @@ public class Estimator implements EstimatorInterface {
         int dir = 0, nextDir = 0; // NORTH = 0, WEST = 1, SOUTH = 2, EAST = 3
         for (int i = 0; i < T[0].length; i++) {
             // Gets the current position and heading
+            System.out.print(T[0].length);
             pos = i / head;
             dir = i % head;
             for (int j = 0; j < T[0].length; j++) {
                 // Iterates through and adds transition probabilities for every other state.
                 nextPos = j / head;
                 nextDir = j % head;
+                if (pos == 0 && nextPos == 1) {
+                    int c = 1+1;
+                }
                 // What is the probability that the next position and direction will be nextPos and nextDir?
-                if (nextPosIsPossible(pos, nextPos)) {
+                if (nextStateIsPossible(pos, nextPos, nextDir)) {
                     if (dir == nextDir) {
                         prob = 0.7;
                     } else {
                         int[] walls = encounteringWalls(pos, dir);
                         boolean wallEncountered = walls[0] == 1;
                         if (wallEncountered) {
-                            prob = 1 / (head - walls[1]);
+                            prob = 1.0 / (head - walls[1]);
                         } else {
                             prob = 0.3 / (head - walls[1] - 1);
                         }
@@ -139,25 +242,35 @@ public class Estimator implements EstimatorInterface {
         }
     }
 
-    private boolean nextPosIsPossible(int p, int np) {
-        int diff = Math.abs(p - np);
+    private boolean nextStateIsPossible(int p, int np, int nh) {
+        int diff = p - np;
         if (p / cols != np / cols) {
             // p and np are on different rows.
-            return diff == cols;
+            if (diff < 0) {
+                return nh == SOUTH && diff == -4;
+            } else {
+                return nh == NORTH && diff == 4;
+            }
+        } else {
+            // p and np are on the same row.
+            if (diff < 0) {
+                return nh == EAST && diff == -1;
+            } else {
+                return nh == WEST && diff == 1;
+            }
         }
-        // They're on the same row.
-        return diff == 1;
     }
 
 
     private int[] encounteringWalls(int p, int h) {
         int wallEncountered = 0; // 0 if false, 1 otherwise
         int walls = 0;
+        int x = (p-1) / cols;
         if (p - cols < 0) {
             walls++; // NORTH
             if (h == NORTH) wallEncountered = 1;
         }
-        if (p / cols != (p - 1) / cols) {
+        if ((p / cols != (p - 1) / cols) || p - 1 < 0) {
             walls++; // WEST
             if (h == WEST) wallEncountered = 1;
         }
